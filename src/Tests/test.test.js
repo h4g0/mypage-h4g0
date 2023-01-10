0b13f08bbb618ff58073fece7dd0d8ce7555ad3e
h4g0 <afonsohugo24@gmail.com>
2023-01-10 20:29:23 +0000

    added tests

---
+++ src/Tests/test.test.js
@@ -0,0 +1,14 @@
+test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
+  });
+
+
+test('adds 2 + 3 to equal 5', () => {
+    expect(2 + 3).toBe(5);
+  });
+
+
+
+test('adds 7 + 2 to equal 9', () => {
+    expect(7 + 2).toBe(9);
+  });
